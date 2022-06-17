323a650a08b79a602e0d8392a31637d1b6164ec5
deniscalin <97188902+deniscalin@users.noreply.github.com>
2022-06-16 21:07:30 -0700

    Update streamlit_app.py
    Added try/if not/else/except statement to organize code

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,17 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-#Get user input and make API call with input as parameter
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-#Take the json version of the response and normalize it
-fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
-#Output it on the screen as a table
-streamlit.dataframe(fruityvice_normalize)
+#New section to Get user input and make API call with input as parameter
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+      streamlit.error("Please select a fruit to get information")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalize)
+except URLError as e:
+    streamlit.error()
 
 streamlit.stop()
 
